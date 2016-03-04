950af31748ebc0fbb87bbfbd0d5f7c68dcf6d50f
Stephen Magill <stephen.magill@gmail.com>
2016-03-04 09:46:05 -0500

    Code to display the galois logo using the ARM to drive 4 panels connected serially.

---
+++ intro/intro.sdk/intro/Debug/src/helloworld.d
@@ -0,0 +1,47 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h ../../intro_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/sleep.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ intro/intro.sdk/intro/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../intro_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../intro_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../intro_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../intro_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
