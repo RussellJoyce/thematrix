2b3ad8b18305584102b758502c80d513ed37bf87
Stephen Magill <stephen.magill@gmail.com>
2016-03-20 23:51:51 -0400

    fpga_logic works now (pulses LEDs).  We started to transform it into a design that uses a Block RAM, but Vivado crashed.  As a result, the project may be in a bad state (haven't checked yet).

--- intro/intro.sdk/webtalk/sdk_webtalk.tcl
+++ intro/intro.sdk/webtalk/sdk_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir /home/smagill/fpga/test/intro/intro.sdk/webtalk
+webtalk_init -webtalk_dir /home/smagill/host/thematrix/intro/intro.sdk/webtalk
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 1 11:06:27 2016" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Mar 20 23:46:52 2016" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "SDK v2015.4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2015.4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "amd64" -context "software_version_and_target_device"
@@ -12,9 +12,9 @@ webtalk_add_data -client project -key target_family -value "NA" -context "softwa
 webtalk_add_data -client project -key target_device -value "NA" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "NA" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "NA" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "74gjt9aunavomsjqeh2vvbhm35" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_id -value "2015.4_2" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "2" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "odvmgb5saosd27qjkasensa8hv" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_id -value "2015.4_5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "" -context "user_environment"
@@ -22,12 +22,12 @@ webtalk_add_data -client project -key cpu_speed -value "" -context "user_environ
 webtalk_add_data -client project -key total_processors -value "" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "" -context "user_environment"
 webtalk_register_client -client sdk
-webtalk_add_data -client sdk -key uid -value "1456844894000" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key isZynq -value "true" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key Processors -value "2" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key VivadoVersion -value "2015.4" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key Arch -value "zynq" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key Device -value "7z010" -context "sdk\\\\hardware/1456844894000"
-webtalk_add_data -client sdk -key IsHandoff -value "true" -context "sdk\\\\hardware/1456844894000"
-webtalk_transmit -clientid 4077893928 -regid "" -xml /home/smagill/fpga/test/intro/intro.sdk/webtalk/usage_statistics_ext_sdk.xml -html /home/smagill/fpga/test/intro/intro.sdk/webtalk/usage_statistics_ext_sdk.html -wdm /home/smagill/fpga/test/intro/intro.sdk/webtalk/sdk_webtalk.wdm -intro "<H3>SDK Usage Report</H3><BR>"
+webtalk_add_data -client sdk -key uid -value "1457539582000" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key isZynq -value "true" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key Processors -value "2" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key VivadoVersion -value "2015.4" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key Arch -value "zynq" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key Device -value "7z010" -context "sdk\\\\hardware/1457539582000"
+webtalk_add_data -client sdk -key IsHandoff -value "true" -context "sdk\\\\hardware/1457539582000"
+webtalk_transmit -clientid 2456690844 -regid "" -xml /home/smagill/host/thematrix/intro/intro.sdk/webtalk/usage_statistics_ext_sdk.xml -html /home/smagill/host/thematrix/intro/intro.sdk/webtalk/usage_statistics_ext_sdk.html -wdm /home/smagill/host/thematrix/intro/intro.sdk/webtalk/sdk_webtalk.wdm -intro "<H3>SDK Usage Report</H3><BR>"
 webtalk_terminate
